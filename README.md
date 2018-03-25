43d2bb8695c1946b40c82487bd44b8ea9c2e03d8
omikankan <omikankan@users.noreply.github.com>
2018-03-25 20:08:21 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # excel-checker
+
+This tool Displays the defined names, styles, sheets name of the specified Excel file.
+Except:
+* reserved names  e.g. Print_Area
+* built-in styles
+
+
+## Usage
+
+1. Execution main.py.
+```
+python C:\excel-checker\src\main.py
+```
+2. After selecting a Excel file, a output dialog will be display.
---
+++ src/main.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+import tkinter.messagebox
+import tkinter.filedialog
+import os
+from openpyxl import load_workbook
+
+
+class FileAttributes:
+    def __init__(self):
+        self.definedNames = ""
+        self.sheetsName = ""
+        self.styles = ""
+
+
+class ExcelData:
+    def __init__(self, path):
+        self.path = path
+        self.fileAttributes = FileAttributes()
+
+    def get_attributes(self):
+        book = load_workbook(self.path)
+
+        self.fileAttributes.definedNames = ""
+        for name in book.defined_names.definedName:
+            if self.fileAttributes.definedNames != "" \
+                    : self.fileAttributes.definedNames = self.fileAttributes.definedNames + "\n"
+            hidden = ""
+            if name.hidden is not None : hidden = " ←hidden"
+            self.fileAttributes.definedNames = self.fileAttributes.definedNames + "  " + name.name + hidden
+
+        self.fileAttributes.sheetsName = ""
+        for sheetname in book.sheetnames:
+            if self.fileAttributes.sheetsName != "" \
+                    : self.fileAttributes.sheetsName = self.fileAttributes.sheetsName + "\n"
+            hidden = ""
+            if book[sheetname].sheet_state == "hidden" : hidden = " ←hidden"
+            self.fileAttributes.sheetsName = self.fileAttributes.sheetsName + "  " + sheetname + hidden
+
+        self.fileAttributes.styles = ""
+        for style in book.style_names:
+            if self.fileAttributes.styles != "" : self.fileAttributes.styles = self.fileAttributes.styles + "\n"
+            self.fileAttributes.styles = self.fileAttributes.styles + "  " + style
+
+        return self.fileAttributes
+
+
+class FileOpenDialog(tkinter.Frame):
+    filetype = [('', "*.xls"), ('', "*.xlsx"), ('', "*.xlsm")]
+    path = ""
+
+    def __init__(self, master):
+        super().__init__(master)
+        master.withdraw()
+        self.path = tkinter.filedialog.askopenfilename(filetypes=self.filetype)
+
+    def open(self):
+        attributes = None
+        if os.path.isfile(self.path) : attributes = ExcelData(self.path).get_attributes()
+        return attributes
+
+
+class OutputDialog:
+    def __init__(self, file_attributes):
+        self.fileAttributes = file_attributes
+
+    def out(self):
+        tkinter.messagebox.showinfo("result",
+        "definedNames : \n" + self.fileAttributes.definedNames + "\n" +
+        "styles : \n" + self.fileAttributes.styles + "\n" +
+        "sheetsName : \n" + self.fileAttributes.sheetsName + "\n")
+
+
+### main ###
+root = tkinter.Tk()
+fileOpenDialog = FileOpenDialog(root)
+root.attributes = fileOpenDialog.open()
+
+if root.attributes is not None:
+    outputDialog = OutputDialog(root.attributes)
+    outputDialog.out()
